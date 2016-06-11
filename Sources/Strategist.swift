f779352595d367dfee72f2b4f9dcef9eacd4a5fa
Vincent Esche <regexident@gmail.com>
2016-06-11 18:55:06 +0200

    Initial commit.

---
+++ Sources/Strategist.swift
@@ -0,0 +1,9 @@
+//
+//  Strategist.swift
+//  Strategist
+//
+//  Created by Vincent Esche on 06/06/16.
+//  Copyright © 2016 Vincent Esche. All rights reserved.
+//
+
+import Darwin
---
+++ Tests/Strategist/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import StrategistTestSuite
+
+XCTMain([
+     testCase(StrategistTests.allTests),
+])
